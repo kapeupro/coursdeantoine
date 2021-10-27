2b2aa88a1e580a1e90aa501a59a2998ca4943ce8
quidelantoine <quidelantoine@gmail.com>
2021-10-27 14:41:16 +0200

    php1 3

--- php/php1/index.php
+++ php/php1/index.php
@@ -221,6 +221,115 @@ switch ($i) {
         echo 'i inconnue';
         break;
 }
+//=====================================
+// Les conditions ---  Opérateurs de comparaison
+//=====================================
+// $a == $b , Egal, TRUE si $a est égal à $b
+// $a === $b , identique,  TRUE si $a est égal à $b et qu'ils sont de même type.
+// 12 == '12' => true
+// 12 === '12' => false
+// $a != $b,  Différent , TRUE si $a est différent de $b
+// $a !== $b,  Différent, TRUE si $a est différent de $b ou bien s'ils ne sont pas du même type.
 
+// $a < $b,    Plus petit que,   TRUE si $a est strictement plus petit que $b.
+// $a > $b  ,  Plus grand,      TRUE si $a est strictement plus grand que $b.
+// $a <= $b ,   Inférieur ou égal,   TRUE si $a est plus petit ou égal à $b.
+// $a >= $b,   Supérieur ou égal,   TRUE si $a est plus grand ou égal à $b.
 
+//=====================================
+// Les conditions ---  Les opérateurs logiques
+//=====================================
+// ET  => &&, AND
+// OU  => ||, OR
+// !  => negation
+$homme = true;
+$age = 43;
+if($homme === true && $age >= 18) {
+
+}
+
+$a = '42';
+$b = ($a === 42); // false
+
+if ($a > 0 && !$b) {
+    echo 'Pomme';
+} elseif ($b && $a <= 42) {
+    echo 'Poire';
+} elseif (!$a || $b > 42) {
+    echo 'Abricot';
+}
+
+if($age >= 18 && $age <= 74) {
+    echo 'Je suis actif';
+}
+echo '<br>';
+//===========================================
+// Arithmétique et incrémentation ----  opérateurs de base (+-/*)
+//===========================================
+// -$a,     Négation ,    Opposé de $a.  // -12
+// $a + $b  ,  Addition ,  Somme de $a et $b.
+// $a - $b,    Soustraction   ,   Différence de $a et $b.
+// $a * $b,    Multiplication,   Produit de $a et $b.
+// $a / $b,    Division,    Quotient de $a et $b.
+// $a % $b  ,   Modulo ,   Reste de $a divisé par $b. Reste de la division
+$chiffre = 3;
+echo  $chiffre % 2; // => 1 => impair
+
+$chiffre2 = 122;
+echo  $chiffre2 % 2; // => 0 => pair
+
+if($chiffre % 2 == 1) {
+    echo 'Le chiffre est pair';
+}
+// Precedence
+$calcul = 4 + 6 * 3; // ??? => 22
+
+//===================
+// Incrémentation -- décrementation
+//===================
+
+// Incrementer
+$i = 1;
+$i++; // $i = $i + 1;  // 2
+echo $i; // 2
+// Décrementer
+$i--;
+echo $i; // 1
+
+$x = 4;
+$x++;
+echo $x; // 5
+
+echo 'La valeur de x avec une post-incrementation est de ' . $x++; // 5
+echo $x; // 6
+echo ' La valeur de x avec une pre-incrementation est de ' . ++$x; // 7
+echo $x; // 7
+echo $x--; // 7
+echo $x; // 6
+
+//===================
+// les opérateurs combinés +=, -=, etc.
+//===================
+// Concaténation égale
+$html = '<h2>';
+$html .= 'Titre de la maison';
+$html .= '</h2>';
+
+//$html2 = '<h2>';
+//$html2 = $html2 . 'Titre de la maison';
+//$html2 = $html2 . '</h2>';
+echo $html;
+////////
+// +=  Plus Egal $x += 2;  $x = $x + 2;
+////////
+$y = 4;
+$y+=12; // $y = $y + 12;
+echo $y; // 16
+////////
+// -=  Moins Egal  $x -= 4;  $x = $x - 4;
+////////
+
+// *=  Multiplie Egal  $x *= 3;  $x = $x * 3;
+// /=  Divise Egal $x /= 2;  $x = $x / 2;
+// %=  Module Egal $x %= 5;  $x = $x % 5;
 
