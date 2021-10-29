d7200f98774d6f29c858cae79327ddb0d00e8aaa
quidelantoine <you@example.com>
2021-10-29 16:57:03 +0200

    php 3 7

--- php/php8/index.php
+++ php/php8/index.php
@@ -6,20 +6,32 @@ require('inc/fonction.php');
 /// /?nom=quidel&prenom=antoine&age=43
 //echo $_GET['nom'];
 //debug($_GET);
-
+$errors = array();
 // existe et n'est pas vide
 if(!empty($_POST['submitted'])) {
-    echo 'OK FORMULAIRE SOUMIS';
+    //echo 'OK FORMULAIRE SOUMIS';
     debug($_POST);
-    echo $_POST['nom'];
+    // Faille XSS
+    $nom = trim(strip_tags($_POST['nom']));
+    $prenom = trim(strip_tags($_POST['prenom']));
+    // Validation
+    // nom
+    if(empty($nom)) {
+        $errors['nom'] = 'Veuillez renseigner ce champ.';
+    }
+
+    // If no error
+    if(count($errors) == 0) {
+        // envoyer un mail
+        // insertion en bdd
+        // redirection
+    }
 }
+debug($errors);
 
 if(!empty($_POST['submitted2']))  {
     echo 'OK FORMULAIRE SOUMIS 222222222';
 }
-
-
-
 include('inc/header.php'); ?>
 
 
@@ -28,9 +40,11 @@ include('inc/header.php'); ?>
     <form action="" method="post" novalidate>
         <label for="nom">Nom *</label>
         <input type="text" name="nom" id="nom" value="<?php if(!empty($_POST['nom'])) {echo $_POST['nom']; } ?>">
+        <span class="error"><?php if(!empty($errors['nom'])) {echo $errors['nom']; } ?></span>
 
         <label for="prenom">Prénom *</label>
-        <input type="text" name="prenom" id="prenom" value="">
+        <input type="text" name="prenom" id="prenom" value="<?php if(!empty($_POST['prenom'])) {echo $_POST['prenom']; } ?>">
+        <span class="error"></span>
 
         <input type="submit" name="submitted" value="Envoyer">
     </form>
