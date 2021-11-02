722e218262216c44c61a235fc2cf671d0a257ce0
quidelantoine <you@example.com>
2021-11-02 16:25:23 +0100

    php 4 5

--- php/php9/detail.php
+++ php/php9/detail.php
@@ -4,19 +4,28 @@ require('brief/data.php');
 
 if(!empty($_GET['id']) && is_numeric($_GET['id'])) {
     $id = $_GET['id'];
-
-
-
-
-
+    // WARNING, NE PAS UTILISER CECI DANS LES PROCHAINS EXOS
+        // NOUS FERONS DES REQUEST A LA BDD A LA PLACE. !!!!!
+    foreach($movies as $film) {
+        if($id === $film['id']) {
+            $currentMovie = $film;
+        }
+    }
+    //debug($currentMovie);
+    if(empty($currentMovie)) {
+        die('404');
+    }
 } else {
     die('404');
 }
 
-
-
 include('inc/header.php');
 
-echo '<h2>Détail d\'un film</h2>';
+    echo '<h2>'.$currentMovie['title'].'</h2>';
+    echo imgMovie($currentMovie);
+    echo '<p>Année: '.$currentMovie['year'] . '</p>';
+    echo '<p>Réal: '.$currentMovie['directors'] . '</p>';
+    echo '<p>Note: '.$currentMovie['rating'] . '</p>';
+    echo '<p>Imdb: '.$currentMovie['imdb_id'] . '</p>';
 
 include('inc/footer.php');
