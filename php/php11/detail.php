da0bcd1637aabceef30bf9d886f6143f314e6f3c
quidelantoine <you@example.com>
2021-11-03 15:24:36 +0100

    php sql 5 2

---
+++ php/php11/detail.php
@@ -0,0 +1,27 @@
+<?php
+
+require('inc/fonction.php');
+require('inc/pdo.php');
+
+if(!empty($_GET['id']) && is_numeric($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM article WHERE id = $id";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    $article = $query->fetch();
+    //debug($article);
+    if(empty($article)) {
+        die('404');
+    }
+} else {
+    die('404');
+}
+
+
+?>
+<div class="article">
+    <h2><?= $article['title']; ?></h2>
+    <p><?= nl2br($article['description']); ?></p>
+    <p class="date"><?= date('d/m/Y',strtotime($article['created_at'])); ?></p>
+</div>
+
---
+++ php/php11/index.php
@@ -0,0 +1,37 @@
+<?php
+require('inc/fonction.php');
+require('inc/pdo.php');
+
+$sql = "SELECT * FROM article ORDER BY created_at DESC";
+// prepare la request
+$query = $pdo->prepare($sql);
+// execute la query
+$query->execute();
+// recuperer les données
+$articles = $query->fetchAll();
+debug($articles);
+
+?>
+<p><a href="">Ajouter un article</a></p>
+<div class="articles">
+    <?php  foreach ($articles as $article) { ?>
+        <div class="article">
+            <h2><?= $article['title']; ?></h2>
+            <p><?= nl2br($article['description']); ?></p>
+            <p class="date"><?= date('d/m/Y',strtotime($article['created_at'])); ?></p>
+            <ul>
+                <li><a href="detail.php?id=<?= $article['id']; ?>">Voir</a></li>
+                <li><a href="">Update</a></li>
+                <li><a href="">Delete</a></li>
+            </ul>
+        </div>
+    <?php } ?>
+</div>
+
+
+
+
+
+
+
+
