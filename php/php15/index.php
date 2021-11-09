f48fc2075a3c4b7c79c26d793deb6332fcf0aa66
quidelantoine <quidelantoine@gmail.com>
2021-11-09 09:32:59 +0100

    php15 1

---
+++ php/php15/index.php
@@ -0,0 +1,33 @@
+<?php
+
+
+// bdd
+    // tables
+        // user
+            // id       (AUTOINCREMENT) INT 11
+            // pseudo      VARCHAR 70
+            // email       VARCHAR 200
+            // password    VARCHAR 255
+            // created_at  DATETIME
+            // role        VARCHAR  20
+            // token       VARCHAR 255
+
+
+
+                // validemail
+                // modified_at
+                // token_at
+
+// index.php
+
+// register.php
+    // => formulaire
+
+        // pseudo (unique, renseigné, min 3, max 60)
+        // email  (unique, renseigné, email valide)
+        // password (x2)  ( identique , min 6 caractères )
+
+// login.php
+    // formulaire
+        // pseudo or email
+        // password
