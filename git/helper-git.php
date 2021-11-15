64e1dd1578c58f96a0ea283103574ff6011f3171
quidelantoine <you@example.com>
2021-11-15 16:23:34 +0100

    php git helper 1

---
+++ git/helper-git.php
@@ -0,0 +1,40 @@
+<?php
+
+// git init
+// git status
+
+
+// git add index.php
+// git add --all
+// git add .
+
+//  git commit -m "git init"
+
+// git config --list --show-origin
+// git config --global user.name "quidelantoine"
+// git config --global user.email "quidelantoine@gmail.com"
+
+
+// .gitignore
+
+// git log --oneline
+// git log --pretty=oneline
+// git log -3
+
+// git remote -v
+
+// git push
+
+// git pull
+
+
+//git branch
+// git branch dev
+// git checkout dev
+// git checkout master
+/////////////// en etant sur master
+// git merge dev
+
+// git branch -d dev
+
+// git clone
