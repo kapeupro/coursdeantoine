8082adf490e9d6f340bf4665d9074d2367c20779
quidelantoine <quidelantoine@gmail.com>
2021-12-06 17:06:28 +0100

    js 5 jquery 1 6

--- js/jquery4/asset/js/main.js
+++ js/jquery4/asset/js/main.js
@@ -1,1 +1,14 @@
-console.log('todo list')
+const input = $('#input');
+const liste = $('#liste');
+
+$("#submitted").on('click', function(e) {
+    e.preventDefault();
+    // recupere la value dans input
+    let data = input.val().trim();
+    if(data !== '') {
+        liste.prepend('<li>' + data + ' <input type="checkbox"></li>');
+        input.val('');
+    } else {
+        console.log('input vide');
+    }
+})
