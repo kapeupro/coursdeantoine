d6dce6545d4c1d7e9070b68f934458dc5eff1ca0
quidelantoine <you@example.com>
2021-12-07 16:58:20 +0100

    js jour 5 4

--- js/jquery5/asset/js/main.js
+++ js/jquery5/asset/js/main.js
@@ -29,10 +29,51 @@ $(window).on('load',function() {
         }
     });
 
+    flexArticleStart();
+    // add article
+    $('#voirplus').on('click', function(e) {
+        e.preventDefault();
 
+        let html = '<div class="article">\n' +
+            '                <div class="article_text">\n' +
+            '                    <h2>titre article 1</h2>\n' +
+            '                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam autem, dignissimos dolorum et eum facilis hic id, iusto laboriosam nemo non placeat quas tempore vitae voluptas! Alias dignissimos doloremque, ex nam necessitatibus nesciunt officia quaerat, quasi, quibusdam quis quos temporibus!</p>\n' +
+            '                </div>\n' +
+            '                <div class="article_diapo">\n' +
+            '                    <div class="flexslider flexArticle">\n' +
+            '                        <ul class="slides">\n' +
+            '                            <li><img src="https://picsum.photos/id/122/300/200" /></li>\n' +
+            '                            <li><img src="https://picsum.photos/id/123/300/200" /></li>\n' +
+            '                        </ul>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '            </div>\n' +
+            '\n' +
+            '            <div class="article">\n' +
+            '                <div class="article_text">\n' +
+            '                    <h2>titre article 1</h2>\n' +
+            '                    <p>Lorem ipsum dolor sit amet, consej kj hdfk gkdfjghkjdf hgkjdfh gkj dfshkj ctetur adipisicing elit. Aperiam autem, dignissimos dolorum et eum facilis hic id, iusto laboriosam nemo non placeat quas tempore vitae voluptas! Alias dignissimos doloremque, ex nam necessitatibus nesciunt officia quaerat, quasi, quibusdam quis quos temporibus!</p>\n' +
+            '                </div>\n' +
+            '                <div class="article_diapo">\n' +
+            '                    <div class="flexslider flexArticle">\n' +
+            '                        <ul class="slides">\n' +
+            '                            <li><img src="https://picsum.photos/id/124/300/200" /></li>\n' +
+            '                            <li><img src="https://picsum.photos/id/125/300/200" /></li>\n' +
+            '                        </ul>\n' +
+            '                    </div>\n' +
+            '                </div>\n' +
+            '            </div>';
+        // enlever le btn
+        $(this).fadeOut(500);
+        // ajouter à la fin de #articles => append
+        $('#articles').append(html);
+        flexArticleStart();
+    });
+});
+
+function flexArticleStart()
+{
     $('.flexArticle').flexslider({
         controlNav: false,
     });
-
-
-});
+}
