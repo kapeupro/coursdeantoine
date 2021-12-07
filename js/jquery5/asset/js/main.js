65582cdf88fff8d1ff30b2cba671c31e114f9524
quidelantoine <you@example.com>
2021-12-07 14:48:24 +0100

    js jour 5 2

---
+++ js/jquery5/asset/js/main.js
@@ -0,0 +1,32 @@
+console.log('plug jquery')
+
+$(window).on('load',function() {
+    $('#flexslider1').flexslider({
+        directionNav: false,
+    });
+
+    $('#flexslider2').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 15,
+        controlNav: false,
+
+    });
+
+
+    const burger = $('#burger');
+    const nav = $('#navigation');
+    burger.on('click', function() {
+        if(burger.hasClass('open')) {
+            burger.removeClass('open');
+            nav.fadeOut(500);
+            burger.children('i').removeClass('fa-times')
+        } else {
+            burger.addClass('open');
+            nav.fadeIn(500);
+            burger.children('i').addClass('fa-times')
+        }
+    });
+
+});
